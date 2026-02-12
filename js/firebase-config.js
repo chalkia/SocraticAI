03685988e83e38ed3fd22b980b1110e2dd0a15c1
chalkia <chalkia.duck@gmail.com>
2026-02-12 12:45:25 +0200

    Create firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,10 @@
+// js/firebase-config.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyDk9EGFJ-R8bsFgEhCvlqv9rO4zRbpw_jU",
+  authDomain: "socraticai-51dd1.firebaseapp.com",
+  projectId: "socraticai-51dd1",
+  storageBucket: "socraticai-51dd1.firebasestorage.app",
+  messagingSenderId: "383761924743",
+  appId: "1:383761924743:web:4aa1522fa25b845ede6e22",
+  measurementId: "G-XXZP8DHYTF"
+};
